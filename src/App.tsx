045afdaf8428d54af1820623e8d398070ef130b8
Zainab Fatima <zainabfatima.zaman@gmail.com>
2026-02-18 12:43:47 +0530

    App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import TownMap from './Home/TownMap';
 import BoardGame from './Board/BoardGame';
-
+import WelcomePage from './Home/WelcomePage';
 function App() {
   console.log("Reached app.tsx")
   return (
     
     <Router>
       <Routes>
-        <Route path="/" element={<TownMap />} />
-        
+        <Route path="/" element={<WelcomePage />} />
+        <Route path="/town" element={<TownMap />} />
         <Route path="/board" element={<BoardGame />} />
       </Routes>
     </Router>
